f3aad5a12e65b2f55f4dd02f1f1db7b43977fb71
xuzhenyang85 <moonniuniu@gmail.com>
2017-03-29 16:42:46 +0200

    added person.java

---
+++ NetBeansGitDemo/src/main/java/Person.java
@@ -0,0 +1,15 @@
+
+public class Person
+{
+    private String firstname;
+    private String lastname;
+    private String cpr;
+
+    @Override
+    public String toString()
+    {
+        return "Person{" + "firstname=" + firstname + ", lastname=" + lastname + ", cpr=" + cpr + '}';
+    }
+    
+    
+}
